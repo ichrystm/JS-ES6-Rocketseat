b6fab22eba67e222298653b6e1f095922d81e5dd
ichrystm <ichrystm@gmail.com>
2020-04-02 11:06:40 -0300

    Ex1 - Att enunciado

--- Modulo_1/main_ex1.js
+++ Modulo_1/main_ex1.js
@@ -1,3 +1,14 @@
+/*
+Exercício 1:
+
+Crie uma classe com nome "Admin", essa classe deveextender uma segunda classe chamada "Usuario".
+A classe usuário deve receber dois parâmetros no método construtor, e-mail e senha, e anotá-losem propriedades da classe.
+A classe "Admin" por sua vez não recebe parâmetros mas deverepassar os parâmetros de e-mail e senha à classe pai e marcar
+uma propriedade "admin" como true na classe.
+Agora com suas classes formatadas, adicione um método na classe Usuario chamado isAdmin queretorna se o usuário é 
+administrador ou não baseado na propriedade admin ser true ou não.
+*/
+
 class usuario {
     constructor(email, senha){
         this.email = email;
