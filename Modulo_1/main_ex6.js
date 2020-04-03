2ef05899faf2b6fb843874918e33f41a3b57d549
ichrystm <ichrystm@gmail.com>
2020-04-03 13:08:38 -0300

    Exercícios 6 e 7

---
+++ Modulo_1/main_ex6.js
@@ -0,0 +1,5 @@
+//Converta o trecho de código utilizando Template Literals
+const usuario = 'Chrysler';
+const idade = 20;
+
+console.log (`O usuário ${usuario} possui ${idade} anos`);
---
+++ Modulo_1/main_ex7.js
@@ -0,0 +1,11 @@
+//Utilize a sintaxe curta de objetos (Object Short Syntax) no objeto
+const nome = 'Chrysler';
+const idade = 20;
+
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul',
+};
+
+console.log(usuario);
